745c5e0e4e6997982235493a8bb53c7a332c618e
Volodymyr Vasyliev <vvasyliev@kse.org.ua>
2026-06-22 04:09:40 +0300

    Added two p_functions, but log is not working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Callable
+import math
 
 import numpy as np
 
@@ -9,11 +10,26 @@ from src import SimulationConfig, EigenvalueSimulator
 def log(n: int) -> float:
     return np.log(n)
 
+""" 
+def logarithm(base: float = math.e) -> Callable[[float], float]:
+    def p_func(n: int) -> float:
+        return math.log(n, base)
+    p_func.__name__ = f"logarithm"
+    return p_func
+"""
+
 def power_scaling(beta: float) -> Callable[[int], float]:
     def p_func(n: int) -> float:
         return n ** beta
     p_func.__name__ = f"power_b{beta}"
     return p_func
+
+def linear(a: float, b: float) -> Callable[[int], float]:
+    def p_func(n: int) -> float:
+        return a * n + b
+    p_func.__name__ = f"linear_a={a}, b={b}"
+    return p_func
+
 
 
 # Entry point
@@ -24,10 +40,10 @@ if __name__ == "__main__":
     for n in n_values:
         config = SimulationConfig(
             n          = n,
-            p_func     = power_scaling(1/2),
+            p_func     = power_scaling(0.9), 
             epsilon    = 0.005,
             batch_size = 5000,
-            sweep_tag  = "power_0.5_test"
+            sweep_tag  = "power_0.9_test"
         )
 
         print(f"Launching simulation  sweep={config.sweep_tag}  n={config.n}")
@@ -38,4 +54,4 @@ if __name__ == "__main__":
 
         results.append(result)
 
-    print("Finished simulation!")
+    print("Finished simulation!") 
